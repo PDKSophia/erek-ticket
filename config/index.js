08833c02a6265e8a7ca86eb4a1085d4b758ba5cc
DK_Peng <1063137960@qq.com>
2019-02-28 16:42:05 +0800

    feat: 采用css module模块化

--- config/index.js
+++ config/index.js
@@ -47,7 +47,35 @@ const config = {
     }
   },
   defineConstants: {},
-  weapp: {},
+  weapp: {
+    module: {
+      postcss: {
+        autoprefixer: {
+          enable: true,
+          config: {
+            browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8']
+          }
+        },
+        pxtransform: {
+          enable: true,
+          config: {}
+        },
+        url: {
+          enable: true,
+          config: {
+            limit: 10240 // 设定转换尺寸上限
+          }
+        },
+        cssModules: {
+          enable: true, // 默认为 false，如需使用 css modules 功能，则设为 true
+          config: {
+            namingPattern: 'module', // 转换模式，取值为 global/module
+            generateScopedName: '[name]__[local]___[hash:base64:5]'
+          }
+        }
+      }
+    }
+  },
   h5: {
     publicPath: '/',
     staticDirectory: 'static',
