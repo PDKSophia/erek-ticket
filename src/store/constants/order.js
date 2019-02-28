77e2e291fc617ec3f73e3c255e228d08c53e1abb
DK_Peng <1063137960@qq.com>
2019-02-28 16:45:52 +0800

    fix: redux的修复

--- src/store/constants/order.js
+++
@@ -1,8 +0,0 @@
-/**
- * @Author: PDK
- * @Date:   2018-09-08
- * @Last modified by:   PDK
- * @Last modified time: 2018-09-08
- */
-export const REQUEST_PAY_ORDER = 'REQUEST_PAY_ORDER'
-export const FROM_CINEMA_TO_ORDER = 'FROM_CINEMA_TO_ORDER'
